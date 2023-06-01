9fcaa842f47546a3c1e0fd6569170fd9976b90cc
Tatoyasha <lord.oximei@gmail.com>
2023-06-01 23:17:11 +0300

    Lab-4 _2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar, Nav, FormControl, Container, Form, Button, Modal } from 'react-bootstrap';
 import logo from './logo192.png';
 
@@ -12,6 +12,60 @@ const Header = () => {
   const handleCloseModal = () => {
     setShowModal(false);
   };
+  
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [emailDirty, setEmailDirty] = useState( false)
+  const [passwordDirty, setPasswordDirty] = useState( false)
+  const [emailError, setEmailError] = useState( 'Email не може бути порожнім')
+  const [passwordError, setPasswordError] = useState( 'Пароль не може бути порожнім')
+  const [formValid, setFormValid] = useState(false)
+  
+  const emailHandler = (e) => {
+    setEmail(e.target.value)
+    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!re.test(String(e.target.value.toLowerCase()))) {
+        setEmailError('Некоректний email')
+    } else {
+        setEmailError('')
+    }
+  }
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value)
+    if (e.target.value.length < 3 || e.target.length > 8) {
+        setPasswordError('Пароль повинен мати не менше 3 і не більше 8 символів')
+        if (!e.target.value) {
+            setPasswordError('Пароль не може бути порожнім')
+        }
+    } else {
+        setPasswordError('')
+    }
+  }
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'email':
+        setEmailDirty(true)
+        break
+      case 'password':
+        setPasswordDirty(true)
+        break
+      default:
+        //Обробка виключень
+        break
+    }
+  }
+  useEffect ( () => {
+    if (emailError || passwordError) {
+        setFormValid(false)
+    } else {
+        setFormValid(true)
+    }
+  },[emailError, passwordError])
+
+
+
 
   return (
     <>
@@ -38,6 +92,8 @@ const Header = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      
+      
 
       <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
@@ -47,7 +103,8 @@ const Header = () => {
           <Form>
             <Form.Group controlId="fromBasicEmail">
                 <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                {(emailDirty && emailError) && <div style={{color:"red"}}>{emailError}</div>}
+                <Form.Control onChange={e => emailHandler(e)} name="email" value={email} onBlur={e => blurHandler(e)} type="email" placeholder="Enter email" />
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                 </Form.Text>
@@ -55,17 +112,20 @@ const Header = () => {
 
             <Form.Group controlId="fromBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter password">
+                {(passwordError && passwordDirty) && <div style={{color:"red"}}>{passwordError}</div>}
+                <Form.Control onChange={e => passwordHandler(e)} name="password" value={password} onBlur={e => blurHandler(e)} type="password" placeholder="Enter password">
                 </Form.Control>
             </Form.Group>
+
 
             <Form.Group controlId="fromBasicCheckbox">
                 <Form.Check type="checkbox" label="Remember me"/>
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button disabled={!formValid} variant="primary" type="submit">
                 Submit
             </Button>
+
         </Form>
 
         }</Modal.Body>
